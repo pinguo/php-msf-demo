e098f80807d77511e4d5cd4e9937693715127556
pinguo-xudianyang <xudianyang@camera360.com>
2017-06-29 15:50:52 +0800

    * rpc示例

--- config/docker/redis.php
+++ config/docker/redis.php
@@ -15,4 +15,25 @@ $config['redis']['tw']['port']             = 6379;
 // 设置key的前缀
 //$config['redis']['tw']['keyPrefix']      = 'hotpot_';
 
+$config['redis']['tw1']['ip']               = '127.0.0.1';
+$config['redis']['tw1']['port']             = 6380;
+
+$config['redis']['read_only']['ip']         = '127.0.0.1';
+$config['redis']['read_only']['port']       = 6381;
+
+$config['redis_proxy']['cluster'] = [
+    'pools' => [
+        'tw'  => 1,
+        'tw1' => 1,
+    ],
+    'mode' => \PG\MSF\Marco::CLUSTER,
+];
+
+$config['redis_proxy']['master_slave'] = [
+    'pools' => [
+        'tw', 'read_only',
+    ],
+    'mode' => \PG\MSF\Marco::MASTER_SLAVE,
+];
+
 return $config;
--- server.php
+++ server.php
@@ -13,6 +13,7 @@ define('APPLICATION_ENV', $_ENV['MSF_ENV'] ?? 'docker');
 define('SYSTEM_NAME', 'demo');
 define("WWW_DIR", realpath(dirname(__FILE__) . '/..'));
 define('RUNTIME_DIR', WWW_DIR . '/runtime/' . SYSTEM_NAME);
+define('DUMP_TRUNCATED', true);
 !is_dir(RUNTIME_DIR) && @mkdir(RUNTIME_DIR, 0755, true);
 
 require_once __DIR__ . '/vendor/autoload.php';
