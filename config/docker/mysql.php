b001542a0432d853910b7278e99e815d77335d4f
xudianyang <120343758@qq.com>
2017-09-14 14:07:36 +0800

    + MYSQL示例

--- config/docker/mysql.php
+++ config/docker/mysql.php
@@ -2,11 +2,11 @@
 /**
  * Docker环境
  */
-$config['mysql']['test']['host']           = '192.168.1.106';
-$config['mysql']['test']['port']           = 6379;
-$config['mysql']['test']['user']           = 'newadv';
-$config['mysql']['test']['password']       = 'ZuQLWj6FhXvTZJrC';
+$config['mysql']['test']['host']           = '127.0.0.1';
+$config['mysql']['test']['port']           = 3306;
+$config['mysql']['test']['user']           = 'root';
+$config['mysql']['test']['password']       = '123456';
 $config['mysql']['test']['charset']        = 'utf8';
-$config['mysql']['test']['database']       = 'paygw';
+$config['mysql']['test']['database']       = 'demo';
 
 return $config;
