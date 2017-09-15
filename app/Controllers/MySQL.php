6afd87cde77360779d37df7a34cdb8d10b3009d9
xudianyang <120343758@qq.com>
2017-09-15 10:55:27 +0800

    + MySQL Proxy

--- app/Controllers/MySQL.php
+++ app/Controllers/MySQL.php
@@ -18,7 +18,7 @@ class MySQL extends Controller
     public function actionBizLists()
     {
         // SQL DBBuilder更多参考 https://github.com/jstayton/Miner
-        $bizLists  = yield $this->getMysqlPool('test')->select("*")->from('biz')->go();
+        $bizLists  = yield $this->getMysqlPool('master')->select("*")->from('biz')->go();
         $this->outputJson($bizLists);
     }
 
@@ -28,7 +28,7 @@ class MySQL extends Controller
         /**
          * @var \PG\MSF\Pools\Miner $DBBuilder
          */
-        $dbs = yield $this->getMysqlPool('test')->go(null, 'show databases');
+        $dbs = yield $this->getMysqlPool('master')->go(null, 'show databases');
         $this->outputJson($dbs);
     }
 
@@ -38,7 +38,7 @@ class MySQL extends Controller
         /**
          * @var \PG\MSF\Pools\Miner|\PG\MSF\Pools\MysqlAsynPool $mysqlPool
          */
-        $mysqlPool = $this->getMysqlPool('test');
+        $mysqlPool = $this->getMysqlPool('master');
         // 开启一个事务，并返回事务ID
         $id = yield $mysqlPool->coroutineBegin();
         $up = yield $mysqlPool->update('user')->set('name', '徐典阳-1')->where('id', 3)->go($id);
@@ -48,6 +48,38 @@ class MySQL extends Controller
             $this->outputJson('commit');
         } else {
             yield $mysqlPool->coroutineRollback($id);
+            $this->outputJson('rollback');
+        }
+    }
+
+    // MySQL代理使用示例
+    public function actionProxy()
+    {
+        /**
+         * @var \PG\MSF\Pools\Miner|\PG\MSF\Pools\MysqlAsynPool $mysqlProxy
+         */
+        $mysqlProxy = $this->getMysqlProxy('master_slave');
+        $bizLists   = yield $mysqlProxy->select("*")->from('user')->go();
+        $up         = yield $mysqlProxy->update('user')->set('name', '徐典阳-6')->where('id', 3)->go();
+        $this->outputJson($bizLists);
+    }
+
+    // MySQL代理事务，事务只会在主节点上执行
+    public function actionProxyTransaction()
+    {
+        /**
+         * @var \PG\MSF\Pools\Miner|\PG\MSF\Pools\MysqlAsynPool $mysqlProxy
+         */
+        $mysqlProxy = $this->getMysqlProxy('master_slave');
+        // 开启一个事务，并返回事务ID
+        $id = yield $mysqlProxy->coroutineBegin();
+        $up = yield $mysqlProxy->update('user')->set('name', '徐典阳-2')->where('id', 3)->go($id);
+        $ex = yield $mysqlProxy->select('*')->from('user')->where('id', 3)->go($id);
+        if ($ex['result']) {
+            yield $mysqlProxy->coroutineCommit($id);
+            $this->outputJson('commit');
+        } else {
+            yield $mysqlProxy->coroutineRollback($id);
             $this->outputJson('rollback');
         }
     }
--- config/docker/mysql.php
+++ config/docker/mysql.php
@@ -2,11 +2,33 @@
 /**
  * Docker环境
  */
-$config['mysql']['test']['host']           = '127.0.0.1';
-$config['mysql']['test']['port']           = 3306;
-$config['mysql']['test']['user']           = 'root';
-$config['mysql']['test']['password']       = '123456';
-$config['mysql']['test']['charset']        = 'utf8';
-$config['mysql']['test']['database']       = 'demo';
+$config['mysql']['master']['host']            = '127.0.0.1';
+$config['mysql']['master']['port']            = 3306;
+$config['mysql']['master']['user']            = 'root';
+$config['mysql']['master']['password']        = '123456';
+$config['mysql']['master']['charset']         = 'utf8';
+$config['mysql']['master']['database']        = 'demo';
+
+$config['mysql']['slave1']['host']           = '127.0.0.1';
+$config['mysql']['slave1']['port']           = 3306;
+$config['mysql']['slave1']['user']           = 'root';
+$config['mysql']['slave1']['password']       = '123456';
+$config['mysql']['slave1']['charset']        = 'utf8';
+$config['mysql']['slave1']['database']       = 'demo';
+
+$config['mysql']['slave2']['host']           = '127.0.0.1';
+$config['mysql']['slave2']['port']           = 3306;
+$config['mysql']['slave2']['user']           = 'root';
+$config['mysql']['slave2']['password']       = '123456';
+$config['mysql']['slave2']['charset']        = 'utf8';
+$config['mysql']['slave2']['database']       = 'demo';
+
+$config['mysql_proxy']['master_slave'] = [
+    'pools' => [
+        'master' => 'master',
+        'slaves' => ['slave1', 'slave2'],
+    ],
+    'mode' => \PG\MSF\Marco::MASTER_SLAVE,
+];
 
 return $config;
