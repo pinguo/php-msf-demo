3d703c3cd9028497a6094eed051b6bb0058bf531
xudianyang <120343758@qq.com>
2017-09-16 13:44:52 +0800

    + MySQL同步模式示例

--- app/Controllers/MySQL.php
+++ app/Controllers/MySQL.php
@@ -11,6 +11,7 @@
 namespace App\Controllers;
 
 use PG\MSF\Controllers\Controller;
+use App\Tasks\Demo as DemoTask;
 
 class MySQL extends Controller
 {
@@ -25,9 +26,6 @@ class MySQL extends Controller
     // 直接执行sql
     public function actionShowDB()
     {
-        /**
-         * @var \PG\MSF\Pools\Miner $DBBuilder
-         */
         $dbs = yield $this->getMysqlPool('master')->go(null, 'show databases');
         $this->outputJson($dbs);
     }
@@ -35,19 +33,16 @@ class MySQL extends Controller
     // 事务示例
     public function actionTransaction()
     {
-        /**
-         * @var \PG\MSF\Pools\Miner|\PG\MSF\Pools\MysqlAsynPool $mysqlPool
-         */
         $mysqlPool = $this->getMysqlPool('master');
         // 开启一个事务，并返回事务ID
-        $id = yield $mysqlPool->coroutineBegin();
+        $id = yield $mysqlPool->goBegin();
         $up = yield $mysqlPool->update('user')->set('name', '徐典阳-1')->where('id', 3)->go($id);
         $ex = yield $mysqlPool->select('*')->from('user')->where('id', 3)->go($id);
         if ($ex['result']) {
-            yield $mysqlPool->coroutineCommit($id);
+            yield $mysqlPool->goCommit($id);
             $this->outputJson('commit');
         } else {
-            yield $mysqlPool->coroutineRollback($id);
+            yield $mysqlPool->goRollback($id);
             $this->outputJson('rollback');
         }
     }
@@ -55,9 +50,6 @@ class MySQL extends Controller
     // MySQL代理使用示例
     public function actionProxy()
     {
-        /**
-         * @var \PG\MSF\Pools\Miner|\PG\MSF\Pools\MysqlAsynPool $mysqlProxy
-         */
         $mysqlProxy = $this->getMysqlProxy('master_slave');
         $bizLists   = yield $mysqlProxy->select("*")->from('user')->go();
         $up         = yield $mysqlProxy->update('user')->set('name', '徐典阳-6')->where('id', 3)->go();
@@ -67,20 +59,61 @@ class MySQL extends Controller
     // MySQL代理事务，事务只会在主节点上执行
     public function actionProxyTransaction()
     {
-        /**
-         * @var \PG\MSF\Pools\Miner|\PG\MSF\Pools\MysqlAsynPool $mysqlProxy
-         */
         $mysqlProxy = $this->getMysqlProxy('master_slave');
         // 开启一个事务，并返回事务ID
-        $id = yield $mysqlProxy->coroutineBegin();
+        $id = yield $mysqlProxy->goBegin();
         $up = yield $mysqlProxy->update('user')->set('name', '徐典阳-2')->where('id', 3)->go($id);
         $ex = yield $mysqlProxy->select('*')->from('user')->where('id', 3)->go($id);
         if ($ex['result']) {
-            yield $mysqlProxy->coroutineCommit($id);
+            yield $mysqlProxy->goCommit($id);
             $this->outputJson('commit');
         } else {
-            yield $mysqlProxy->coroutineRollback($id);
+            yield $mysqlProxy->goRollback($id);
             $this->outputJson('rollback');
         }
+    }
+
+    // 通过Task，同步执行MySQL查询（连接池）
+    public function actionSyncMySQLPoolTask()
+    {
+        /**
+         * @var DemoTask $demoTask
+         */
+        $demoTask = $this->getObject(DemoTask::class);
+        $user     = yield $demoTask->syncMySQLPool();
+        $this->outputJson($user);
+    }
+
+    // 通过Task，同步执行MySQL查询（代理）
+    public function actionSyncMySQLProxyTask()
+    {
+        /**
+         * @var DemoTask $demoTask
+         */
+        $demoTask = $this->getObject(DemoTask::class);
+        $user     = yield $demoTask->syncMySQLProxy();
+        $this->outputJson($user);
+    }
+
+    // 通过Task，同步执行MySQL事务查询（连接池）
+    public function actionSyncMySQLPoolTaskTransaction()
+    {
+        /**
+         * @var DemoTask $demoTask
+         */
+        $demoTask = $this->getObject(DemoTask::class);
+        $user     = yield $demoTask->syncMySQLPoolTransaction();
+        $this->outputJson($user);
+    }
+
+    // 通过Task，同步执行MySQL事务查询（代理）
+    public function actionSyncMySQLProxyTaskTransaction()
+    {
+        /**
+         * @var DemoTask $demoTask
+         */
+        $demoTask = $this->getObject(DemoTask::class);
+        $user     = yield $demoTask->syncMySQLProxyTransaction();
+        $this->outputJson($user);
     }
 }
---
+++ app/Tasks/Demo.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Tasks;
+
+use \PG\MSF\Tasks\Task;
+
+class Demo extends Task
+{
+    /**
+     * 连接池执行同步查询
+     *
+     * @return array
+     */
+    public function syncMySQLPool()
+    {
+        $user = $this->getMysqlPool('master')->select("*")->from("user")->go();
+        return $user;
+    }
+
+    /**
+     * 代理执行同步查询
+     *
+     * @return array
+     */
+    public function syncMySQLProxy()
+    {
+        $user = $this->getMysqlProxy('master_slave')->select("*")->from("user")->go();
+        return $user;
+    }
+
+    /**
+     * 连接池执行同步事务
+     *
+     * @return boolean
+     */
+    public function syncMySQLPoolTransaction()
+    {
+        $mysqlPool = $this->getMysqlPool('master');
+        $id = $mysqlPool->begin();
+        // 开启一个事务，并返回事务ID
+        $up = $mysqlPool->update('user')->set('name', '徐典阳-1')->where('id', 3)->go($id);
+        $ex = $mysqlPool->select('*')->from('user')->where('id', 3)->go($id);
+        if ($ex['result']) {
+            $mysqlPool->commit();
+            return true;
+        } else {
+            $mysqlPool->rollback();
+            return false;
+        }
+    }
+
+    /**
+     * 代理执行同步事务
+     *
+     * @return boolean
+     */
+    public function syncMySQLProxyTransaction()
+    {
+        $mysqlPool = $this->getMysqlProxy('master_slave');
+        $id = $mysqlPool->begin();
+        // 开启一个事务，并返回事务ID
+        $up = $mysqlPool->update('user')->set('name', '徐典阳-1')->where('id', 3)->go($id);
+        $ex = $mysqlPool->select('*')->from('user')->where('id', 3)->go($id);
+        if ($ex['result']) {
+            $mysqlPool->commit();
+            return true;
+        } else {
+            $mysqlPool->rollback();
+            return false;
+        }
+    }
+}
