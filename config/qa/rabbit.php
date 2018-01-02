735cd5874e841ff786409bafc1e8acb4314ef5fa
niulingyun <niulingyun@camera360.com>
2018-01-02 15:09:48 +0800

    qa

---
+++ config/qa/rabbit.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * RabbitMQ 配置
+ * @author lingyun <niulingyun@camera360.com>
+ * @copyright Chengdu pinguo Technology Co.,Ltd.
+ * Date: 27/10/2017
+ */
+
+$config['amqp'] = [
+    'rabbit' => [
+        'host' => '127.0.0.1',
+        'port' => '5672'
+    ]
+];
+
+return $config;
